06b643b4c716b6700b1542f395d792b0db0535ef
PaulBredl <paulbredl@gmx.de>
2024-05-28 21:52:02 +0200

    update stores

--- src/stores/appStore.ts
+++ src/stores/appStore.ts
@@ -1,21 +1,26 @@
-import { defineStore } from 'pinia'
 import { useLocalStorage } from '@vueuse/core'
 
-export const useAppStore = defineStore('login', {
-  state: () => ({
-    projectId: useLocalStorage('projectId', null as string | null)
-  }),
-  getters: {
-    getProjectId(): string | null {
-      return this.projectId
-    },
-    isProjectSelected(): boolean {
-      return this.projectId !== null
+// remark: there are store management libraries like Vuex or Pinia, however,
+// the Vue 3 composition API is powerful enough to manage global state
+// and I had more trouble with the libraries than without them.
+// However, if the project grows, it might be a good idea to use a store management library
+
+const projectId = useLocalStorage('projectId', null as string | null)
+
+const getProjectIdOrThrow = () => {
+  if (projectId.value === null) {
+    throw new Error('No project selected')
+  }
+  return projectId.value;
+}
+
+export function useAppStore() {
+
+  return {
+    projectId,
+    getProjectIdOrThrow,
+    isProjectSelected() {
+      return projectId.value !== null
     }
-  },
-  actions: {
-    setProjectId(projectId: string) {
-      this.projectId = projectId
-    },
-  },
-})
+  }
+}
